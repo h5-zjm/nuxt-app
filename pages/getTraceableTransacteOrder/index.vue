45d1f41ac119ebf7b7c5028ccb4d29bacf7abb74
denggang <707012087@qq.com>
2020-08-18 04:27:55 +0800

    功能完成

--- pages/getTraceableTransacteOrder/index.vue
+++ pages/getTraceableTransacteOrder/index.vue
@@ -140,11 +140,12 @@
 			};
 		},
 		created() {
-			this.getData()
+			
 		},
 		onLoad: function (option) { //option为object类型，会序列化上个页面传递的参数
 			console.log(option); //打印出上个页面传递的参数。
 			this.orderID = option.id
+			this.getData()
 		},
 		methods:{
 			getData() {
